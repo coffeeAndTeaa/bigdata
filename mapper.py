dd7322f35736bbcb33e3759a73db9121f90e237a
eigen <jingyu.pang@tufts.edu>
2022-02-21 20:59:55 -0500

    fix

--- mapper.py
+++ mapper.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
 
 import sys
+import os
+
 dict = {}
 
-with open('./AFINN-en-165.txt') as f:
+with open('~/AFINN-en-165.txt') as f:
 	lines = f.readlines()
 	for line in lines:
 		line = line.strip()
@@ -12,10 +14,20 @@ with open('./AFINN-en-165.txt') as f:
 		key = " ".join(words)
 		dict[key] = value 
 
+# file_name = os.getenv('map_input_file')
+# names = file_name.split('_')
+# name =  file_name[0]
+name = 'wilson'
+word_number = 0
+valence = 0
 for line in sys.stdin:
 	line = line.strip()
 	words = line.split()
 
 	for word in words:
+		word_number += 1
 		if word in dict:
-			print('%s\t%s' %(word, dict[word]))
+			valence += int(dict[word])
+
+valence = valence / (word_number / 1000.0)
+print('%s\t%s' %(name, valence))
